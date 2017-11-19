9b0b520cf864045a336b5b4b2967681c75f015ea
Teoxishere <teodor.chiretu@gmail.com>
2017-11-19 17:00:53 +0200

    adding stuff

---
+++ DrugStoreReloaded/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DrugStoreReloaded.Startup))]
+namespace DrugStoreReloaded
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
